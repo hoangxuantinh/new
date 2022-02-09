f01d7faf8f2a70850d82653a52065ff64c5f451b
hxtinh99 <hxt2849@gmail.com>
2022-02-09 14:28:56 +0700

    fix upload file

--- project2/backend/src/middleware/uploadFile.js
+++ project2/backend/src/middleware/uploadFile.js
@@ -1,9 +1,14 @@
 import multer from 'multer';
 import appRoot from 'app-root-path';
+import fs from 'fs-extra';
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, `${appRoot}/public/images/`);
+        const path = `${appRoot}/public/images/`;
+        fs.ensureDir(path, (err) => {
+            if (err) return console.log(err);
+        });
+        cb(null, path);
     },
     filename(req, file, cb) {
         const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
